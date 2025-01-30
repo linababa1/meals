135c4b0f68dc4dd03ce1e0616914ed65b1e7480d
linababa1 <linababa987@gmail.com>
2025-01-30 10:16:47 +0200

    build

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
-    seedColor: const Color.fromARGB(255, 131, 57, 0),
+    seedColor: const Color.fromARGB(256, 131, 57, 0),
   ),
   textTheme: GoogleFonts.latoTextTheme(),
 );
